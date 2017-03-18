f7df009969c69aca6801bec1613f2f27d8d68980
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2017-03-18 03:20:47 -0500

    Comment out files for gitignore

---
+++ application/logs/2017-03-18.log.php
@@ -0,0 +1,4 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2017-03-18 06:19:22 +00:00 --- error: Uncaught PHP Error: Non-static method protochart::chart() should not be called statically, assuming $this from incompatible context in file application/controllers/admin/dashboard.php on line 116
+2017-03-18 06:22:18 +00:00 --- error: Uncaught PHP Error: Non-static method html::escape() should not be called statically, assuming $this from incompatible context in file themes/default/views/header_nav.php on line 17
