594e81f70016176618c284966ca665df5ff7e324
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2017-03-18 11:46:27 -0400

    Update changes

--- application/logs/2017-03-18.log.php
+++ application/logs/2017-03-18.log.php
@@ -2,3 +2,5 @@
 
 2017-03-18 06:19:22 +00:00 --- error: Uncaught PHP Error: Non-static method protochart::chart() should not be called statically, assuming $this from incompatible context in file application/controllers/admin/dashboard.php on line 116
 2017-03-18 06:22:18 +00:00 --- error: Uncaught PHP Error: Non-static method html::escape() should not be called statically, assuming $this from incompatible context in file themes/default/views/header_nav.php on line 17
+2017-03-18 08:21:45 +00:00 --- error: Uncaught Kohana_Database_Exception: Unknown Exception: database.table_not_found in file application/libraries/drivers/Database/Mysql.php on line 316
+2017-03-18 03:28:19 -05:00 --- error: Excepción no atrapada Kohana_Exception: The upload destination folder, /var/www/html/huaycoperu2017.tk/media/uploads/, does not appear to be writable. en el archivo application/helpers/upload.php en la línea 58
