c2768e9292607e24fa2224c18b57f4d2b57a182c
Claudia Marquez <claudia92@gmail.com>
2017-03-19 08:45:02 -0500

    ultimas incidencias template

--- themes/emergenciaperu/views/main/timeline.php
+++ themes/emergenciaperu/views/main/timeline.php
@@ -1,7 +1,6 @@
 <div class="slider-holder">
 	<?php echo form::open(NULL, array('method' => 'get')); ?>
 		<input type="hidden" value="0" name="currentCat" id="currentCat"/>
-		<fieldset>
 			<!-- HT: More info link -->
 			<a class="f-clear" href="#" id="timelineMoreLink" style="font-size: 11px;"><?php echo Kohana::lang('ui_main.more_information'); ?></a>
 			<!-- HT: Manual time interval for timeline select input -->
@@ -12,7 +11,6 @@
 			<select name="startDate" id="startDate"><?php echo $startDate; ?></select>
 			<label for="endDate"><?php echo Kohana::lang('ui_main.to'); ?>:</label>
 			<select name="endDate" id="endDate"><?php echo $endDate; ?></select>
-		</fieldset>
 	<?php echo form::close(); ?>
 </div>
 <?php if (Kohana::config('settings.enable_timeline')): ?>
