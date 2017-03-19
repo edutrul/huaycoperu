3a6c2fac28f3044deff764e8a4dc87d035a5cd50
Renzo Telenta <rtelenta@caleidos.pe>
2017-03-19 13:06:10 -0500

    cambios

--- themes/emergenciaperu/views/alerts/main.php
+++ themes/emergenciaperu/views/alerts/main.php
@@ -75,7 +75,7 @@
 							</div>
 						</div>
 					</div>
-					<input id="btn-send-alerts" class="btn_submit" type="submit" value="<?php echo Kohana::lang('ui_main.alerts_btn_send'); ?>" />
+					<input id="btn-send-alerts" class="btn_submit btn red" type="submit" value="<?php echo Kohana::lang('ui_main.alerts_btn_send'); ?>" />
 					<BR /><BR />
 					<a href="<?php echo url::site()."alerts/confirm";?>"><?php echo Kohana::lang('ui_main.alert_confirm_previous'); ?></a>
 				</div>
